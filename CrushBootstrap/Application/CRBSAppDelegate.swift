3543cca3c30b44f4494c2c03b8e8792edf8d3c21
Zitao Xiong <caoer115@gmail.com>
2016-07-11 23:02:16 -0400

    app delegate

---
+++ CrushBootstrap/Application/CRBSAppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  CRBSAppDelegate.swift
+//  CrushBootstrap
+//
+//  Created by Zitao Xiong on 7/11/16.
+//  Copyright © 2016 Crush & Lovely. All rights reserved.
+//
+
+import UIKit
+
+class CRBSAppDelegate: UIResponder {
+
+}
