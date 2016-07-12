65e71581f3d98aedda331328971ce241aaf047cd
Zitao Xiong <caoer115@gmail.com>
2016-07-11 23:09:02 -0400

    swiftify

---
+++ CrushBootstrap/Controllers/CRBSViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CRBSViewController.swift
+//  CrushBootstrap
+//
+//  Created by Zitao Xiong on 7/11/16.
+//  Copyright © 2016 Crush & Lovely. All rights reserved.
+//
+
+import UIKit
+
+class CRBSViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
